b010e08e9bbe459c961ec34b173359289a806fd3
bengys <bshear13@gmail.com>
2016-12-13 08:56:17 +0200

    Basic programme functionality

--- reverseshell.sh
+++ reverseshell.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
-lsof -i | grep bash |awk '{print $2}' | tail > pids.txt
-if [ -s pids.txt ]
-  then
-	shell_pid="$(python -c "import rs; rs.getPID();")"
-	echo $shell_pid
-	strace -p$shell_pid -o keylog.txt -e trace=read > /dev/null &
-	python -c "import rs; rs.parse_keylog();"
-fi
+:> keylog.txt
+:> rawkey.txt
+:> pids.txt
+shell_detected=false
+while [ "$shell_detected" = false ] ; do	
+	lsof -i | grep -w -e 'bash' -e 'sh' | awk '{print $2}'> pids.txt
+	if [ -s pids.txt ] ; then
+		shell_detected=true
+		shell_pid="$(python -c "import rs; rs.getPID();")"
+		echo $shell_pid
+		sleep 1
+	fi
+done
+sleep 1
+strace -p$shell_pid -o rawkey.txt -e trace=read & > /dev/null
+
+process_on="null"
+while [ "$process_on" != "" ] ; do
+	process_on="$(ps -A | grep $shell_pid)"
+	if [ -s rawkey.txt ] ; then
+		python -c "import rs; rs.parse_rawkeys();"
+	fi
+done
+
+echo "DONE"
--- rs.py
+++ rs.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 def getPID():
 	pid_file_reader = open("pids.txt",'r')
@@ -8,23 +9,28 @@ def getPID():
 	print(rs_pid)
 	return rs_pid
 	
-def parse_keylog():
-	rk_reader = open('rawkey.txt','w+')
-	kl_reader = open('keylog.txt','w+')
+def parse_rawkeys():
+	rk_file = open('rawkey.txt','r')
+	kl_file = open('keylog.txt','a')
+	
 	#list to store collection of lines
 	keylog = []
 	#list to store line
 	line_buf = []
-	for line in kl_reader.readlines():
-		if(line[0:4] == 'read'):
-			word=line.split("\"")[1]
+
+	#read in each line from file, if the line is a 'read' call then 
+	#extract the read input	
+	for line in rk_file.readlines():
+		if ('\"' in line and line.find('-')):
+			search_obj = re.search(r'[^.]*\"([^.]*)\"',line) 
+			if(search_obj):
+				word = search_obj.group(1)
 			if  word != "\\n":
 				line_buf.append(word)
 			else:
-				keylog.append(''.join(line_buf))
-				line_buf[:] = []	
-	keylog.append(''.join(line_buf))
-	for line in keylog:
-		print(line)		
-	rk_reader.close()
-	kl_reader.close()
+				kl_file.write(''.join(line_buf)+'\n')
+				line_buf[:] = []			
+	kl_file.write(''.join(line_buf))
+	rk_file.close()
+	kl_file.close()
+	open('rawkey.txt','w').close
